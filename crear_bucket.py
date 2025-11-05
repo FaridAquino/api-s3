11857dc1046f97ae682b5810d8987973fbcc0c56
FaridAquino <farid.aquino@utec.edu.pe>
2025-11-05 16:26:07 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -2,7 +2,7 @@ import boto3
 
 def lambda_handler(event, context):
     try:
-        bucket_name= event['bucket_name']
+        bucket_name= event['body']['bucket_name']
         s3_client = boto3.client('s3', region_name='us-east-1')
         s3_client.create_bucket(Bucket=bucket_name)
 
