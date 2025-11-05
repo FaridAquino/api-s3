7bd9af5e346ed62e1f4dac7235e545b5537a7b92
FaridAquino <farid.aquino@utec.edu.pe>
2025-11-05 16:26:19 -0500

    Update crear_directorio_bucket.py

--- crear_directorio_bucket.py
+++ crear_directorio_bucket.py
@@ -2,8 +2,8 @@ import boto3
 
 def lambda_handler(event, context):
     try:
-        bucket_name = event['bucket_name']
-        directory_name = event['directory_name']
+        bucket_name = event['body']['bucket_name']
+        directory_name = event['body']['directory_name']
         s3_client = boto3.client('s3', region_name='us-east-1')
 
         s3_client.put_object(Bucket=bucket_name, Key=(directory_name+'/'))
